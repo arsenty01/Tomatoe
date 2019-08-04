e6878b567f6264750f99c7f89a720ff5a0a332d8
Arsenty Semenenko <arsenty@live.com>
2019-08-05 01:34:39 +0300

    rework

--- start.py
+++ start.py
@@ -1,185 +1,92 @@
 from main_window import Ui_MainWindow
 from timeout_window import Ui_timeout_window
-from PyQt5 import QtWidgets, QtCore, QtGui
+from datetime import datetime
+from PyQt5 import QtWidgets, QtCore
 import sys
-import json
 import time
-import _thread
-
-saveTimer = 0
 
 
-class ModalWindow(QtWidgets.QDialog):
+class MainWindow(QtWidgets.QMainWindow):
+    """ main window """
+
     def __init__(self):
-        QtWidgets.QDialog.__init__(self)
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.child = TimeoutWindow(self)
+        self.ui.setupUi(self)
+        self.ui.btnStart.clicked.connect(self.session_start)
+        self.settings = {"Session": "09:00:00", "Work": "00:00:10", "Rest": "00:00:10"}
+
+    def session_start(self):
+        """ start to count """
+        self.session_thead = WorkThread(self.settings)
+        self.session_thead.show_window.connect(self.start_rest)
+        self.session_thead.update_gui.connect(self.update_gui)
+
+        self.session_thead.start()
+
+    def start_rest(self, data=False):
+        """ start to rest """
+        # TODO figure out about the window
+        print(data)
+        if data:
+            self.child.exec_()
+        else:
+            self.child.close()
+
+    def update_gui(self, data):
+        """ start to count """
+        # TODO create updating method
+        print(data)
+
+
+class WorkThread(QtCore.QThread):
+    """ session """
+
+    show_window = QtCore.pyqtSignal(bool)
+    update_gui = QtCore.pyqtSignal(dict)
+    time_format = "%H:%M:%S"
+
+    def __init__(self, settings):
+        super().__init__()
+        self.settings = settings
+
+    def run(self):
+
+        self.zero_point = datetime.strptime("00:00:00", self.time_format)
+        self.end_point = datetime.strptime(self.settings["Session"], self.time_format)
+        self.work_period = datetime.strptime(self.settings["Work"], self.time_format)
+        self.rest_period = datetime.strptime(self.settings["Rest"], self.time_format)
+
+        self.session_delta = self.end_point - self.zero_point
+        self.work_delta = self.work_period - self.zero_point
+        self.rest_delta = self.rest_period - self.zero_point
+
+        self.median = self.session_delta.seconds // (self.work_delta.seconds + self.rest_delta.seconds)
+        global_timer = 0
+        for i in range(self.median):
+            for w in range(self.work_delta.seconds):
+                self.update_gui.emit({"m": time.strftime(self.time_format, time.gmtime(global_timer)),
+                                      "a": time.strftime(self.time_format, time.gmtime(w))})
+                global_timer += 1
+                time.sleep(1)
+            self.show_window.emit(True)
+            # TODO how to manage user close the window
+            time.sleep(self.rest_delta.seconds)
+            self.show_window.emit(False)
+
+
+class TimeoutWindow(QtWidgets.QDialog):
+    """ timeout window """
+
+    def __init__(self, parent=None):
+        super().__init__(parent, QtCore.Qt.Window)
         self.ui = Ui_timeout_window()
         self.ui.setupUi(self)
-        self.profiles = Application.get_profiles()
-        self.setWindowIcon(QtGui.QIcon('tom.png'))
-        self.timeoutDuration = Application.qtime_to_seconds(QtCore.QTime.fromString(self.profiles['Default']['timeout']))
-        _thread.start_new_thread(self.hello, ())
-        self.show()
-        self.exec()
-
-    def hello(self):
-        global saveTimer
-        y = 0
-        saveTimer = 0
-        zeroPoint = QtCore.QTime.fromString('00:00:00', 'hh:mm:ss')
-        while self.timeoutDuration >= y:
-            time.sleep(1)
-            zeroPoint = zeroPoint.addSecs(1)
-            self.ui.ldTimeoutTimer.setText(zeroPoint.toString())
-            y += 1
-            saveTimer = y
-        self.close()
 
 
-class Application(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Application, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.profiles = self.get_profiles()
-        self.session_flag = False
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
-        self.ui.sessionTimeEdit.setTime(QtCore.QTime.fromString(self.profiles['Default']['duration'], 'hh:mm:ss'))
-        self.ui.worktimeTimeEdit.setTime(QtCore.QTime.fromString(self.profiles['Default']['workTime'], 'hh:mm:ss'))
-        self.ui.timeoutTimeEdit.setTime(QtCore.QTime.fromString(self.profiles['Default']['timeout'], 'hh:mm:ss'))
-        self.ui.lbTimerSession.setText(self.profiles['Default']['duration'])
-        self.ui.lbTimerTimeout.setText(self.profiles['Default']['workTime'])
-        self.ui.editButton.clicked.connect(self.edit_settings)
-        self.ui.applyButton.clicked.connect(self.save_settings)
-        self.ui.btnStart.clicked.connect(self.action)
-        self.ui.btnStop.clicked.connect(self.stop_session)
+app = QtWidgets.QApplication([])
+application = MainWindow()
+application.show()
 
-    def action(self):
-        self.ui.btnStart.setEnabled(False)
-        self.ui.btnStop.setEnabled(True)
-        _thread.start_new_thread(self.start_session, ())
-
-    def stop_session(self):
-        self.session_flag = False
-        self.ui.btnStart.setEnabled(True)
-        self.ui.btnStop.setEnabled(False)
-
-    def edit_settings(self):
-        self.ui.applyButton.setEnabled(True)
-        self.ui.sessionTimeEdit.setEnabled(True)
-        self.ui.worktimeTimeEdit.setEnabled(True)
-        self.ui.timeoutTimeEdit.setEnabled(True)
-        self.ui.editButton.setEnabled(False)
-
-    @staticmethod
-    def get_profiles():
-        try:
-            with open('profiles.json') as f:
-                profiles = json.load(f)
-        except FileNotFoundError:
-            new_profiles = {
-                "Default": {
-                    'duration': '09:00:00',
-                    'workTime': '00:45:00',
-                    'timeout': '00:15:00'
-                }
-            }
-            with open('profiles.json', 'w') as f:
-                json.dump(new_profiles, f)
-            with open('profiles.json') as f:
-                profiles = json.load(f)
-        return profiles
-
-    def save_settings(self):
-        duration = self.ui.sessionTimeEdit.time().toString()
-        workTime = self.ui.worktimeTimeEdit.time().toString()
-        timeout = self.ui.timeoutTimeEdit.time().toString()
-        name = 'Default'
-        self.profiles[name] = {
-            'duration': duration,
-            'workTime': workTime,
-            'timeout': timeout
-        }
-        with open('profiles.json', 'w') as f:
-            json.dump(self.profiles, f)
-        self.ui.lbTimerTimeout.setText(workTime)
-        self.ui.lbTimerSession.setText(duration)
-        self.ui.applyButton.setEnabled(False)
-        self.ui.sessionTimeEdit.setEnabled(False)
-        self.ui.worktimeTimeEdit.setEnabled(False)
-        self.ui.timeoutTimeEdit.setEnabled(False)
-        self.ui.editButton.setEnabled(True)
-
-    @staticmethod
-    def qtime_to_seconds(qtime):
-        hours = qtime.hour()
-        minutes = qtime.minute()
-        seconds = qtime.second()
-        return hours * 3600 + minutes * 60 + seconds
-
-    def start_session(self):
-        global saveTimer
-        self.ui.editButton.setEnabled(False)
-        self.session_flag = True
-        current_profile = 'Default'
-        zero_point = QtCore.QTime.fromString('00:00:00', 'hh:mm:ss')
-        end_point = QtCore.QTime.fromString('23:59:59', 'hh:mm:ss')
-        duration_time = QtCore.QTime.fromString(self.profiles[current_profile]['duration'])
-        session_counter = zero_point
-        workTime_counter = zero_point
-
-        workTime = QtCore.QTime.fromString(self.profiles[current_profile]['workTime'])
-        workTime_seconds = self.qtime_to_seconds(workTime)
-
-        timeout_i = 0
-        while session_counter < duration_time:
-            if self.session_flag is False:
-                self.ui.lbTimerSession.setText(self.profiles[current_profile]['duration'])
-                self.ui.lbTimerTimeout.setText(self.profiles[current_profile]['workTime'])
-                break
-            timeout_i += 1
-            session_counter = session_counter.addSecs(1)
-            workTime_counter = workTime_counter.addSecs(1)
-            if timeout_i == workTime_seconds:
-                self.childwindow()
-                session_counter = session_counter.addSecs(saveTimer)
-                workTime_counter = zero_point
-                timeout_i = 0
-            time.sleep(1)
-            self.ui.lbTimerSession.setText(session_counter.toString())
-            self.ui.lbTimerTimeout.setText(workTime_counter.toString())
-
-        timeout_e = 0
-        self.ui.lbTimerTimeout.setText(self.profiles[current_profile]['workTime'])
-        self.ui.lbTimerSession.setStyleSheet('font: bold 20px;color: red;')
-        while session_counter != end_point:
-            if self.session_flag is False:
-                self.ui.lbTimerSession.setText(self.profiles[current_profile]['duration'])
-                self.ui.lbTimerTimeout.setText(self.profiles[current_profile]['workTime'])
-                break
-            timeout_e += 1
-            session_counter = session_counter.addSecs(1)
-            time.sleep(1)
-            self.ui.lbTimerSession.setText(session_counter.toString())
-        self.ui.lbTimerSession.setText(self.profiles[current_profile]['duration'])
-        self.ui.lbTimerSession.setStyleSheet('font: bold 20px;color: black;')
-        self.ui.editButton.setEnabled(True)
-        self.ui.btnStart.setEnabled(True)
-        self.ui.btnStop.setEnabled(False)
-
-    def childwindow(self):
-        self.window = ModalWindow()
-
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    application = Application()
-    application.setWindowIcon(QtGui.QIcon('tom.png'))
-
-    application.show()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
-
+sys.exit(app.exec())
